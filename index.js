974cbc7c66833b8f46d1babd2af2965b4a20aa35
jortsupetterson <lehtinenjori03@gmail.com>
2026-02-21 23:12:39 +0200

    chore: testing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { generateVerificationPair } from '@z-base/cryptosuite'
-import { create, merge } from './dist/index.js'
+import { createHistory, mergeHistories, updateHistory } from './dist/index.js'
 const issuer = 'f27ec28f-5f21-486f-96d2-3ef81f8e1bab'
 const { verifyJwk, signJwk } = await generateVerificationPair()
 
-const genesis = await create(issuer, verifyJwk, signJwk)
+const genesis = await createHistory(issuer, verifyJwk, signJwk)
 
-console.log(genesis)
+updateHistory(genesis, { mitä: 'botti' }, signJwk)
 
-merge(genesis, await create(issuer, verifyJwk, signJwk))
+mergeHistories(genesis, await createHistory(issuer, verifyJwk, signJwk))
 
 console.log(genesis)
